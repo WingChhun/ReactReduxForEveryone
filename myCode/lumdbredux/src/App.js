62e367907b3e4ae463033d990f96041a5465bd42
WingChhun <chhunjamesdc2@yahoo.com>
2018-04-06 13:55:08 -0700

    End of #6, import Provider, create store with test hello reducer

--- myCode/lumdbredux/src/App.js
+++ myCode/lumdbredux/src/App.js
@@ -1,30 +1,37 @@
 /* eslint react/no-did-mount-set-state: 0 */
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//React-Router
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import logo from "./logo.svg";
+import "./App.css";
 
-import MoviesList from './MoviesList';
-import MovieDetail from './MovieDetail';
+//Import components for movies
+
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
+//Create store
+const hello = () => "hello james";
+const store = createStore(hello);
+
+//APP COMPONENT
 const App = () => (
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/">
-          <img src={logo} className="App-logo" alt="logo" />
-        </Link>
-      </header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </div>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </Link>
+        </header>
+        <Switch>
+          <Route exact path="/" component={MoviesList} />
+          <Route path="/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
