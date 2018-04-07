c26bef63701d87ffe11439a48527d1a78dfbef39
WingChhun <chhunjamesdc2@yahoo.com>
2018-04-07 12:27:43 -0700

    Import action type constnats from action creator file, as well as change case for TOGGLE_MESSAGE to GET_MOVIES, to test loading movie API data

--- myCode/lumdbredux/src/reducers/reducer.js
+++ myCode/lumdbredux/src/reducers/reducer.js
@@ -1,16 +1,23 @@
+//IMPORT ACTION TYPE CONSTANTS
+import { TOGGLE_MESSAGE, GET_MOVIES } from "../actions/actions";
+
 //initialState is the fallback state
 const initialState = {
-  messageVisibility: false
+  messageVisibility: false,
+  movies: []
 };
 
 export default function(state = initialState, action) {
-  const { type } = action;
+  //Pull action.type and action.data from action
+  const { type, data } = action;
   switch (type) {
-    case "TOGGLE_MESSAGE":
+    case TOGGLE_MESSAGE:
       return {
         ...state,
         messageVisibility: !state.messageVisibility
       };
+    case GET_MOVIES:
+      return { ...state, movies: data };
     default:
       return state;
   }
