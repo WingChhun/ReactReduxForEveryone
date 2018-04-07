98798ba4bcd8a2472240af951bf3a3ac7d448dc5
WingChhun <chhunjamesdc2@yahoo.com>
2018-04-07 12:25:43 -0700

    End of #15, thunk and redux-logger, created middleWare array passed into composeWithDevTools()

--- myCode/lumdbredux/src/App.js
+++ myCode/lumdbredux/src/App.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 //React-Router
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import thunk from 'redux-thunk';
+//Extra styking
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -13,14 +16,20 @@ import MoviesList from "./MoviesList";
 import MovieDetail from "./MovieDetail";
 import Toggle from "./components/Toggle";
 //import root reducers
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers/root_reducers";
+
+//END IMPORTS
+
+//MIDDLEWARE
+const MIDDLEWARE = [logger, thunk];
 //Create store
-const store = createStore(rootReducer, 
-{},
-composeWithDevTools()
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...MIDDLEWARE))
 );
-console.log("State",store.getState);
+console.log("State", store.getState);
 //APP COMPONENT
 const App = () => (
   <Provider store={store}>
@@ -31,7 +40,7 @@ const App = () => (
             <img src={logo} className="App-logo" alt="logo" />
           </Link>
         </header>
-        <Toggle/>
+        <Toggle />
         <Switch>
           <Route exact path="/" component={MoviesList} />
           <Route path="/:id" component={MovieDetail} />
